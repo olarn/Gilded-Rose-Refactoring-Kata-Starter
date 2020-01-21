2b0440bf2287f8ae0d1c699bf4c9593b4f2514ed
Olarn U <olarn.u@gmail.com>
2020-01-21 14:56:56 +0700

    fix typo

--- GlideRoseRefactor.Console/Program.cs
+++ GlideRoseRefactor.Console/Program.cs
@@ -18,7 +18,7 @@ namespace GlideRoseRefactor.Console
                     new Item { Name = "Elixir of the Mongoose",     SellIn = 5,  Quality = 7},
                     new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0,  Quality = 80},
                     new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20},
-                    new Item {Name = "Conjured Mana Cake",          SellIn = 3,  Quality = 6}
+                    new Item { Name = "Conjured Mana Cake",          SellIn = 3,  Quality = 6}
                 }
             };
             app.UpdateQuality();
