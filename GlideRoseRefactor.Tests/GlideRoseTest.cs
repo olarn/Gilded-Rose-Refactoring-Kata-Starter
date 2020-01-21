c3656bf05e66c87c9ba583838e931b0604cfaf93
Olarn U <olarn.u@gmail.com>
2020-01-21 14:34:55 +0700

    Add require files into project.

---
+++ GlideRoseRefactor.Tests/GlideRoseTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using GlideRoseRefactor.Console;
+
+namespace GlideRoseRefactor.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            throw new NotImplementedException("Test not implement yet.");
+        }
+    }
+}
